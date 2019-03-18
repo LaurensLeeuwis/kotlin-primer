5b69aae1009218917f2b2aaab37b64fd4a95ab1c
Laurens Leeuwis <laurens.leeuwis@philips.com>
2019-03-18 10:17:35 +0100

    add readable function name

--- src/main/kotlin/eu/leeuwis/kotlinprimer/part07/Lazy values.kt
+++ src/main/kotlin/eu/leeuwis/kotlinprimer/part07/Lazy values.kt
@@ -1,13 +1,13 @@
 package eu.leeuwis.kotlinprimer.part07
 
-fun slowFunction(): String {
+fun `slow function`(): String {
     Thread.sleep(3000)
     return "Slow stuff"
 }
 
 fun main() {
-    val slow = slowFunction()
-//    val slow by lazy {slowFunction()}
+    val slow = `slow function`()
+//    val slow by lazy {`slow function`()}
 
     println("this will take some time")
     println("because this is $slow")
