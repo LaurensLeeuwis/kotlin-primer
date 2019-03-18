2ea9cd20a0ae57f266dc5523ae039d7dc93ec101
Laurens Leeuwis <laurens.leeuwis@philips.com>
2019-03-18 08:53:01 +0100

    extend nullability

--- src/main/kotlin/eu/leeuwis/kotlinprimer/part11/Nullability.kt
+++ src/main/kotlin/eu/leeuwis/kotlinprimer/part11/Nullability.kt
@@ -1,5 +1,25 @@
+package eu.leeuwis.kotlinprimer.part11
+
+fun cantPassNull(parameter: String) {
+    println("this is not null: '$parameter'")
+}
+
+fun canPassNull(parameter: String?) {
+    if (parameter == null){
+        println("passed null")
+    } else {
+        cantPassNull(parameter)
+    }
+}
+
 fun main() {
     val aString : String? = null
-//    val anotherString : String = null
+    val anotherString : String? = "I am here!"
+    val certainlyAString : String = "I am a String"
 
+    canPassNull(aString)
+    canPassNull(anotherString)
+
+//    cantPassNull(anotherString)
+    cantPassNull(certainlyAString)
 }
